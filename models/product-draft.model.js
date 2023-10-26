52c5d0b9dc3861dcf3e487cb3469d23f83a8535d
sang.voohuoc <sang.vophuoc@vti.com.vn>
2023-10-26 16:06:10 +0900

    commit draft data

--- models/product-draft.model.js
+++ models/product-draft.model.js
@@ -11,7 +11,6 @@ class ProductModel {
                 }
             })
         })
-	// draft comment for PR check
     }
     static async getProduct(productID){
         return new Promise((resolve, reject) => {
